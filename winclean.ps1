f6125b9fcda3f13d4313ac7343b48bfddce98939
Paul Pfeister <paul@pfeister.dev>
2024-01-28 22:57:48 -0500

    New file

---
+++ winclean.ps1
@@ -0,0 +1,9 @@
+# Sanitizer for new Windows builds
+# github.com/paulpfeister/sysbuild
+#
+# MAINTAINER : Paul Pfeister (github.com/paulpfeister)
+# 
+# PURPOSE    : Eliminate much of the crapware that comes preinstalled on Windows 10 and Windows 11, and disable or
+#              mitigate certain baked-in telemetry items, to the greatest extent possible without breaking Windows.
+#
+# WARRANTY   : See license.
